ef4f0204f6448e6307364c75928d002a1ebad5ab
kimdaesik <ds500033@naver.com>
2024-08-22 14:08:03 +0900

    mission1

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import data from "../assets/data/movieListData.json"
 import MovieCard from "../components/MovieCard";
-import { Pagination } from 'swiper/modules';
+import { Pagination, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from "swiper/react";
 import 'swiper/css';
 
@@ -14,12 +14,31 @@ function Main() {
         <div className="inner">
           <h2 className="section-title">평점순 TOP 20</h2>
           <Swiper
-            slidesPerView={3}
+            slidesPerView={1}
             spaceBetween={30}
+            // navigation={true}
+            // navigation={{
+            //   prevEl: '.custom-prev',
+            //   nextEl: '.custom-next',
+            // }}
             pagination={{
               clickable: true,
             }}
-            modules={[Pagination]}
+            breakpoints={{
+              512:{
+                slidesPerView: 2,
+              },
+              768:{
+                slidesPerView: 3,
+              },
+              1024:{
+                slidesPerView: 4,
+              },
+              1280:{
+                slidesPerView: 5,
+              },
+            }}
+            modules={[Pagination, Navigation]}
             className="mySwiper movie-swiper"
           >
             {
@@ -33,6 +52,8 @@ function Main() {
                 )
               })
             }
+            {/* <div className="custom-prev">이전</div>
+            <div className="custom-next">다음</div> */}
           </Swiper>
           <h2 className="section-title">인기순</h2>
           <ul className="movie-list">
